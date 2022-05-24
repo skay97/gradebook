c69b97b31878eedc7f563e1b6e373c50fc35cdf7
Salman <salmankarimzindani@hotmail.com>
2022-05-24 15:05:38 -0500

    feat: adds test to verify avg grade is successfully calculated

---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,27 @@
+using Xunit;
+using System;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void BookCalculatesAnAverageGrade()
+        {
+            //arrange
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            //Act
+            var result = book.GetStatistics();
+
+            //Assert
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1); 
+
+        }
+    }
+}
