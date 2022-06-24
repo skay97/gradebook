fbcc4dae292e8fdca6cbcd0106de8d2de0fc0ece
Salman <salmankarimzindani@hotmail.com>
2022-06-24 17:03:21 -0500

    enhancement: makes book class an abstract base level class

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -6,96 +6,12 @@ using System.Threading.Tasks;
 
 namespace GradeBook
 {
-    public delegate void GradeAddedDelegate(object sender, EventArgs args);
-
-    public class Book : NamedObject
+    public abstract class Book : NamedObject
     {
-        private List<double> grades;
-        
-        public const string CATEGORY = "Science";
-
-        public event GradeAddedDelegate GradeAdded;
-
-        public Book(string name) : base(name)
+        protected Book(string name) : base(name)
         {
-            grades = new List<double>();
-            Name = name;
         }
 
-        public void AddGrade(char letter)
-        {
-            switch (letter)
-            {
-                case 'A':
-                    AddGrade(90);
-                    break;
-                case 'B':
-                    AddGrade(80);
-                    break;
-                case 'C':
-                    AddGrade(70);
-                    break;
-                default:
-                    AddGrade(0);
-                    break;
-            }
-        }
-
-
-        public void AddGrade(double grade)
-        {
-            if (grade <= 100 && grade >= 0)
-            {
-                grades.Add(grade);
-
-                if (GradeAdded != null)
-                {
-                    GradeAdded(this, new EventArgs());
-                }
-            } else
-            {
-                throw new ArgumentException($"Invalid input: {grade}");
-            }
-        }
-        public Statistics GetStatistics()
-        {
-            var result = new Statistics();
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
-
-            for (var i = 0; i < grades.Count; i++)
-            {
-                result.Low = Math.Min(grades[i], result.Low);
-                result.High = Math.Max(grades[i], result.High);
-                result.Average += grades[i];
-            };
-
-            result.Average /= grades.Count;
-
-            switch (result.Average)
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-
-                default:
-                    result.Letter = 'F';
-                    break;
-            }
-
-            return result;
-        }
+        public abstract void AddGrade(double grade);
     }
 }
